6fc85f9f7d020f62a7bda2f019767c3f140bda6e
Shailja1409 <shailja.singh_cs.h24@gla.ac.in>
2024-11-28 21:08:45 +0530

    remove

---
+++ coding-assignment/list/list_q03.py
@@ -0,0 +1,13 @@
+'''method to remove empty lists from a list'''
+
+a = [[1, 2], [], [3, 4], [], [5]]
+res = []
+
+# Iterate over list 'a'
+for b in a:
+  
+    # If list is not empty then add it to res.
+    if b:
+        res.append(b)
+
+print(res)
