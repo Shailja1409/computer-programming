84c3e315dd7a6094d5c9cd82c30a22b50b3eb65d
Shailja1409 <shailja.singh_cs.h24@gla.ac.in>
2024-11-28 21:22:19 +0530

    unique items

---
+++ coding-assignment/list/list_q09.py
@@ -0,0 +1,15 @@
+'''counting unique items inside a list in Python'''
+
+input_list = [1, 2, 2, 5, 8, 4, 4, 8]
+
+# taking an input list
+l1 = []
+
+count = 0
+
+for item in input_list:
+    if item not in l1:
+        count += 1
+        l1.append(item)
+
+print("No of unique items are:", count)
