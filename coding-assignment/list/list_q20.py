e120184e4fe7500a1b42d6cc4b9bbffbe82b1e62
Shailja1409 <shailja.singh_cs.h24@gla.ac.in>
2024-11-28 21:37:03 +0530

    reverse

---
+++ coding-assignment/list/list_q20.py
@@ -0,0 +1,13 @@
+'''Reverse All Strings in String List'''
+
+test_list = ["geeks", "for", "geeks", "is", "best"]
+ 
+# printing original list
+print ("The original list is : " + str(test_list))
+ 
+# using list comprehension
+# Reverse All Strings in String List
+res = [i[::-1] for i in test_list]
+ 
+# printing result
+print ("The reversed string list is : " + str(res))
