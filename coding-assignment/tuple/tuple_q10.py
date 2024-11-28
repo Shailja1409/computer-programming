ee2ec7a5e9b144eb888311b788cc89e8ca2e76dc
Shailja1409 <shailja.singh_cs.h24@gla.ac.in>
2024-11-28 21:54:41 +0530

    removing duplicates

---
+++ coding-assignment/tuple/tuple_q10.py
@@ -0,0 +1,13 @@
+'''Removing duplicates from tuple'''
+
+test_tup = (1, 3, 5, 2, 3, 5, 1, 1, 3)
+ 
+# printing original tuple 
+print("The original tuple is : " + str(test_tup))
+ 
+# Removing duplicates from tuple 
+# using tuple() + set()
+res = tuple(set(test_tup))
+ 
+# printing result
+print("The tuple after removing duplicates : " + str(res))
